f648c964cd4f81cd90b10afbb879bafcede7a87a
pkc-3 <pkc_3@naver.com>
2021-07-05 17:36:39 +0900

    2021/07/05 파이썬 100문제 76문제까지 완료

--- Q51~Q100/Q69.py
+++ Q51~Q100/Q69.py
@@ -24,19 +24,27 @@ def cal():  # 20000까지의 소수를 전부 호출하는 함수
     return primes
 
 
-a = cal()
+a = cal()  # 소수를 전부 다 넣음 타입은 리스트
+# print(type(a))
 # ==========================================
 n = int(input())  # 테스트할 값 입력
 l = []
 k = []
 
-for i in range(2, n//2+1):
+for i in range(2, n//2+1):  # 2부터 테스트 값을 3으로 나눈값의 몫값 까지 반복실행 500을 입력하면 166까지
+    # i가 소수거나 n-i가 소수면 l 리스트에 넣는다.(i와 n-i 는 한 짝이 된다.) (13,487) ,(37,463),(43,457)이런식으로 l에 쌓임
     if i in a and n-i in a:
         l.append(i)
         l.append(n-i)
 
-for i in range(0, len(l)-1, 2):
-    k.append(l[i+1]-l[i])
+# print(l)
+# 목적은 짝 안에 숫자의 차이가 적어야한다.
+# for i in range(0, len(l)-1, 2): 방법1
+#     k.append(l[i+1]-l[i])
 
-index = k.index(min(k))*2
-print(l[index], l[index+1])
+# index = k.index(min(k))*2
+# print(l[index], l[index+1])
+
+k.append(l.pop())  # 방법2 제일 마지막 숫자 짝이 차가 제일 적을수 밖에 없다.
+k.append(l.pop())
+print(k[-1], k[0])
